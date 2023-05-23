c845a9d1e5cd92f27ae41007a6fb62c13cbaeb67
Oladapo Ahmed Opeyemi <oladapoahmed5@gmail.com>
2023-05-24 00:42:17 +0100

    Wrote basic c code in voting.c

--- voting.c
+++ voting.c
@@ -2,7 +2,15 @@
 
 int main()
 {
-	int prompt;
+	int prompt, age;
 	
-	printf ("Enter Your age");
+	printf("Enter Your age: ");
+	scanf("%d", &age);
+
+	if (age < 18)
+	{
+		printf("Uneligible to vote");
+	}
+
+	return 0;
 }
